08a7e14775c40fd9eeabb43d5545dd44b7504cb9
Christian Fobel <christian@fobel.net>
2018-01-18 17:38:50 -0500

    fix(post-link): do not exec if PKG_NAME not set
    Do not excecute `post-link` script if `PKG_NAME` environment variable is not set.
    
    See [here][1] for more info.
    
    [1]: https://github.com/sci-bots/microdrop/issues/250

--- .conda-recipe/post-link.bat
+++ .conda-recipe/post-link.bat
@@ -1,5 +1,17 @@
 @echo off
-set PLUGIN_NAME=dropbot_plugin
+REM Do not excecute script if `PKG_NAME` environment variable is not set.
+REM
+REM See: https://github.com/sci-bots/microdrop/issues/250
+IF NOT DEFINED PKG_NAME ( 
+  echo Do not excecute script since `PKG_NAME` environment variable is not set.
+  echo See https://github.com/sci-bots/microdrop/issues/250 for more info.
+  exit /b
+)
+
+REM Strip `microdrop.` prefix (i.e., first 10 characters) from package name.
+set PLUGIN_NAME=%PKG_NAME:~10%
+REM Replace hyphen characters with underscores.
+set PLUGIN_NAME=%PLUGIN_NAME:-=_%
 
 REM Link installed plugin into Conda MicroDrop activated plugins directory.
 call "%PREFIX%\Scripts\activate.bat" "%PREFIX%" & python -m mpm.bin.api enable %PLUGIN_NAME%
