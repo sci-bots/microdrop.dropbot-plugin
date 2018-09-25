e7c0b952b61bb409546586d6cb89685dcc537993
Christian Fobel <christian@fobel.net>
2018-09-25 12:20:03 -0400

    chore(license): release under BSD 3-clause license

--- pyutilib.py
+++ pyutilib.py
@@ -1,21 +1,3 @@
-"""
-Copyright 2015-2017 Ryan Fobel and Christian Fobel
-
-This file is part of dropbot_plugin.
-
-dropbot_plugin is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-dropbot_plugin is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with dropbot_plugin.  If not, see <http://www.gnu.org/licenses/>.
-"""
 from __future__ import division
 from concurrent.futures import ThreadPoolExecutor
 from functools import wraps
