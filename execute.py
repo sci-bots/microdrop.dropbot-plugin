cbcb2d50b753dfdf49692007e56069054edc4c6a
Christian Fobel <christian@fobel.net>
2019-06-07 08:31:27 -0400

    fix(execute/actuate): fix actuated area field typo
    Prior to this commit, the key for the actuated area item in capacitance
    update messages for volume threshold actuations was incorrectly spelled
    "acuated_area" (note the first missing "t").
    
    In this commit, fix spelling actuated area key in capacitance update
    messages for volume threshold actuations to be "actuated_area".

--- execute.py
+++ execute.py
@@ -165,7 +165,7 @@ def actuate(proxy, dmf_device, electrode_states, duration_s=0,
             capacitance_messages = dropbot_event['capacitance_updates']
             # Add actuated area to capacitance update messages.
             for capacitance_i in capacitance_messages:
-                capacitance_i['acuated_area'] = actuated_area
+                capacitance_i['actuated_area'] = actuated_area
 
             result['threshold'] = {'target': dropbot_event['target'],
                                    'measured': dropbot_event['new_value'],
