8a20dddfc2f3f97cf98788acc5a9fb642da431e2
Ryan Fobel <ryan@fobel.net>
2020-10-13 15:21:30 -0400

    fix(build): fix activation in pre/post scripts

--- .conda-recipe/pre-unlink.bat
+++ .conda-recipe/pre-unlink.bat
@@ -2,9 +2,9 @@
 set PLUGIN_NAME=dropbot_plugin
 
 REM Unlink installed plugin from Conda MicroDrop activated plugins directory.
-call "%PREFIX%\Scripts\activate.bat" "%PREFIX%" & python -m mpm.bin.api disable %PLUGIN_NAME%
+call conda activate "%PREFIX%" & python -m mpm.bin.api disable %PLUGIN_NAME%
 echo Unlinked `%PLUGIN_NAME%` from MicroDrop activated plugins directory. > "%PREFIX%\.messages.txt"
 
 REM Disable loading of plugin in MicroDrop.
-call "%PREFIX%\Scripts\activate.bat" "%PREFIX%" & microdrop-config edit --remove plugins.enabled %PLUGIN_NAME%
+call conda activate "%PREFIX%" & microdrop-config edit --remove plugins.enabled %PLUGIN_NAME%
 echo Disable loading of `%PLUGIN_NAME%` in MicroDrop. >> "%PREFIX%\.messages.txt"
